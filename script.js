a8aa4c21cf4076f58e0ad7db8fa255c4e74c6980
Benjamin Kloss <Benjaminkloss@outlook.de>
2024-05-16 15:31:03 +0200

    add logic vor button

--- script.js
+++ script.js
@@ -61,16 +61,27 @@ let currentQuestion = 0; // Globale variable in Integer zum anzeigen der Aktuell
 
 function init() {
   document.getElementById("all-questions").innerHTML = questions.length; // damit wird die gesamit länge angezeigt bei Frage blabla von (allen Fragen als integer)
-  showQuestion();
+  showQuestion();// verbindung zur funktion, dadurch wird sie automatisch aufgerufen
 }
 
 
 
 function showQuestion(){        //hilfsfunktion um um aktuelle Frage anzuzeigen in der Init
     let question = questions[currentQuestion]; // neuen container namens question definiert in den die Currentquestion(ganzes JsonArray hineinkopiert wird)
-    document.getElementById('questiontext').innerHTML = `${question['question']}`;
-    document.getElementById('answer_1').innerHTML = `${question['answer_1']}`;
-    document.getElementById('answer_2').innerHTML = `${question['answer_2']}`;
-    document.getElementById('answer_3').innerHTML = `${question['answer_3']}`;
-    document.getElementById('answer_4').innerHTML = `${question['answer_4']}`;
+    document.getElementById('questiontext').innerHTML = question['question']; // ruft nacheinander die einzelnen inhalter der jeweiligen fragen auf genauso wie die Antworten
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
   }
+
+  function answer(selection){//selcetion hat den wert answer_1,2,3 oder 4 
+    let question = questions[currentQuestion]; // um zur aktuellen frage zu kommen
+    let selectedQuestionnumber = selection.slice(-1)// der letzte buchstabe des Strings in dem fall die integer 3 wird gespeichert
+    if(selectedQuestionnumber == question['right_answer']){//wenn die Zahl oben drüber, der Zahl im Json array entspricht dann
+      console.log('right');//gebe mir die richtige antwort aus
+    } else[ // ansonsten, sag es ist die falsche antwort
+      console.log('false')
+    ]
+  
+}
