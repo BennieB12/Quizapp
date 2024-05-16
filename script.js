8b3fe731a95b795c922bab1de7c65e00c621d887
Benjamin Kloss <Benjaminkloss@outlook.de>
2024-05-16 12:53:46 +0200

    added questions

--- script.js
+++ script.js
@@ -1,10 +1,58 @@
-let questions =[
-    {
-     "questions": "Wer hat HTML Erfunden?",
-        "answer_1": "Chuck Norris",
-        "answer_2": "Steve Jobs",
-        "answer_3": "Tim Berners-Lee",
-        "answer_4": "Justin Bieber",
-        "right_answer": 3
-    },
+let questions = [
+  {
+        question: "Wer hat HTML Erfunden?",
+        answer_1: "Chuck Norris",
+        answer_2: "Steve Jobs",
+        answer_3: "Tim Berners-Lee",
+        answer_4: "Justin Bieber",
+        right_answer: 3,
+  },
+  {
+    question: "Wer hat HTML Erfunden?",
+    answer_1: "Chuck Norris",
+    answer_2: "Steve Jobs",
+    answer_3: "Tim Berners-Lee",
+    answer_4: "Justin Bieber",
+    right_answer: 3,
+},
+{
+    question: "Wer hat HTML Erfunden?",
+    answer_1: "Chuck Norris",
+    answer_2: "Steve Jobs",
+    answer_3: "Tim Berners-Lee",
+    answer_4: "Justin Bieber",
+    right_answer: 3,
+},
+{
+    question: "Wer hat HTML Erfunden?",
+    answer_1: "Chuck Norris",
+    answer_2: "Steve Jobs",
+    answer_3: "Tim Berners-Lee",
+    answer_4: "Justin Bieber",
+    right_answer: 3,
+},
+{
+    question: "Wer hat HTML Erfunden?",
+    answer_1: "Chuck Norris",
+    answer_2: "Steve Jobs",
+    answer_3: "Tim Berners-Lee",
+    answer_4: "Justin Bieber",
+    right_answer: 3,
+},
+{
+    question: "Wer hat HTML Erfunden?",
+    answer_1: "Chuck Norris",
+    answer_2: "Steve Jobs",
+    answer_3: "Tim Berners-Lee",
+    answer_4: "Justin Bieber",
+    right_answer: 3,
+},
+{
+    question: "Wer hat HTML Erfunden?",
+    answer_1: "Chuck Norris",
+    answer_2: "Steve Jobs",
+    answer_3: "Tim Berners-Lee",
+    answer_4: "Justin Bieber",
+    right_answer: 3,
+},
 ];
