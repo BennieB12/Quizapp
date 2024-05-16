154a8eeca16f89b4da0e8327f916145c2f0c473b
Benjamin Kloss <Benjaminkloss@outlook.de>
2024-05-16 11:03:20 +0200

    added real first array

---
+++ script.js
@@ -0,0 +1,10 @@
+let questions =[
+    {
+     "questions": "Wer hat HTML Erfunden?",
+        "answer_1": "Chuck Norris",
+        "answer_2": "Steve Jobs",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+];
